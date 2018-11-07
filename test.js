3bef5f908d17a2f2f64001a6b2b7fc872b6b44b5
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-07 12:14:06 +0100

    Fix issue with adding listener after synchronous termination

--- test.js
+++ test.js
@@ -60,3 +60,103 @@ test('it converts from (fake) DOM node events', (t) => {
   }, 700);
 });
 
+test('it handles sink immediately terminating source', (t) => {
+  t.plan(5);
+  const elem = {
+    added: false,
+    id: null,
+
+    addEventListener: (name, listener) => {
+      t.equals(name, 'click', 'addEventListener for click');
+      this.added = true;
+      let i = 0;
+      this.id = setInterval(() => listener((++i)*10));
+    },
+
+    removeEventListener: (name, listener) => {
+      t.equals(name, 'click', 'removeEventListener for click');
+      this.added = false;
+      clearInterval(this.id);
+    }
+  }
+  const source = fromEvent(elem, 'click');
+
+  const downwardsExpectedType = [
+    [0, 'function']
+  ];
+
+  function makeSink(type, data) {
+    let talkback;
+    return (type, data) => {
+      const et = downwardsExpectedType.shift();
+      t.ok(et, 'downwards call was expected');
+      t.equals(type, et[0], 'downwards type is expected: ' + et[0]);
+      t.equals(typeof data, et[1], 'downwards data type is expected: ' + et[1]);
+      if (type === 0) {
+        talkback = data;
+        talkback(2);  // Immediately end.
+      } else {
+        t.fail('incorrectly written test, this should never be called');
+      }
+    };
+  }
+
+  const sink = makeSink();
+  source(0, sink);
+
+  setTimeout(() => {
+    t.pass('nothing else happens after dispose()');
+    t.end();
+  }, 700);
+});
+
+test('immediately terminating sink doesn\'t receive immediately emitted data', (t) => {
+  t.plan(5);
+  const elem = {
+    added: false,
+    id: null,
+
+    addEventListener: (name, listener) => {
+      t.equals(name, 'click', 'addEventListener for click');
+      this.added = true;
+      let i = 0;
+      listener(++i);
+      this.id = setInterval(() => listener((++i)*10));
+    },
+
+    removeEventListener: (name, listener) => {
+      t.equals(name, 'click', 'removeEventListener for click');
+      this.added = false;
+      clearInterval(this.id);
+    }
+  }
+  const source = fromEvent(elem, 'click');
+
+  const downwardsExpectedType = [
+    [0, 'function']
+  ];
+
+  function makeSink(type, data) {
+    let talkback;
+    return (type, data) => {
+      const et = downwardsExpectedType.shift();
+      t.ok(et, 'downwards call was expected');
+      t.equals(type, et[0], 'downwards type is expected: ' + et[0]);
+      t.equals(typeof data, et[1], 'downwards data type is expected: ' + et[1]);
+      if (type === 0) {
+        talkback = data;
+        talkback(2);  // Immediately end.
+      } else {
+        t.fail('incorrectly written test, this should never be called');
+      }
+    };
+  }
+
+  const sink = makeSink();
+  source(0, sink);
+
+  setTimeout(() => {
+    t.pass('nothing else happens after dispose()');
+    t.end();
+  }, 700);
+});
