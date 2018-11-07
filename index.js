ea1ef32a346d38927e01d80a17d976ac0e483beb
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-07 12:14:06 +0100

    Fix issue with adding listener after synchronous termination

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const fromEvent = (node, name) => (start, sink) => {
   if (start !== 0) return;
+  let disposed = false;
   const handler = ev => sink(1, ev);
+
   sink(0, t => {
-    if (t === 2) node.removeEventListener(name, handler);
+    if (t !== 2) {
+      return;
+    }
+    disposed = true;
+    node.removeEventListener(name, handler);
   });
+
+  if (disposed) {
+    return;
+  }
+
   node.addEventListener(name, handler);
 };
 
